92c24fb88d93468dbbd4be9576c90e1888ebe783
Ruhul Amin <ruhuliuice@gmail.com>
2021-12-16 16:49:12 +0600

    added shared folder

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import ContactUs from "./Components/ContactUs/ContactUs";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="contact" element={<ContactUs></ContactUs>}></Route>
+        
       </Routes>
     </div>
   );
